725a5b86afcb6f403859d44347c347b1adc254e8
AgustinGomez <agustinmatiasgomez1998@gmail.com>
2023-01-12 13:30:36 -0300

    primera parte del proyecto

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Products, Navbar } from './components'
+
+
+ 
+const App = () => {
+    return (
+        <div>
+            <Products />
+            <Navbar />
+
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+
+import logo from '../../assets/commerce.png';
+import useStyles from './styles';
+
+const Navbar = () => {
+    const classes = useStyles();
+
+    return (
+        <>
+          <AppBar position="fixed" className={classes.appBar} color="inherit">
+            <Toolbar>
+              <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
+                <img src={logo} alt="commerce.js" height="25px" className={classes.image} /> Commerce.js
+              </Typography>
+              <div className={classes.grow} />
+              {location.pathname === '/' && (
+              <div className={classes.button}>
+                <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+                  <Badge badgeContent={totalItems} color="secondary">
+                    <ShoppingCart />
+                  </Badge>
+                </IconButton>
+              </div>
+              )}
+            </Toolbar>
+          </AppBar>
+          {renderMobileMenu}
+        </>
+    );
+}
+
+export default Navbar;
